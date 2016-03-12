90c0603bfc03374b5ca66c2546ca1edfcb07e1f1
Callan Morrison <callan.l.morrison@gmail.com>
2016-03-12 09:58:44 -0600

    unnested 2nd xhr function and created new function for it

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -2,6 +2,17 @@ var productContainer = document.getElementById("productContainer");
 var seasonalDiscount = document.getElementById("seasonalDiscount");
 
 
+
+
+
+
+
+
+
+
+
+
+
 // PRODUCT XHR
 //Step 1: Set up http req for products
 var myProductsReq = new XMLHttpRequest;
@@ -17,12 +28,16 @@ myProductsReq.addEventListener("failed", failedExecution);
 //Step 4: Translate into JS
 function failedExecution() {
     alert("Error loading page. Please refresh.")
-}
+};
 
+//Step 5: Create callback for once the product page loads
 function productsSuccess() {
     var productData = JSON.parse(this.responseText);
-        
+    loadNextJson2(productData)
+}
 
+//Once first JSON loads, move to load second JSON       
+function loadNextJson2(productData) {
         // DEPARTMENT XHR
         //Step 1: Set up http req for products
         var myDepartmentReq = new XMLHttpRequest;
@@ -39,25 +54,50 @@ function productsSuccess() {
         //Step 4: Translate into JS
         function departmentSuccess() {
             var departmentData = JSON.parse(this.responseText);
+            domOutput(productData, departmentData);
         }
-        
-        domOutput(productData);
 }
 
-
-
-
-function domOutput(productData) {
+function domOutput(productData, departmentData) {
     for (var i = 0; i < productData.products.length; i++) {
         var buildString = "<div class='product'";
         buildString += `<h2> Product: ${productData.products[i].name} </h2>`
         buildString += `<h5> Price: ${productData.products[i].price} </h5>`
-        console.log(buildString);
+        var categoryId = productData.products[i].category_id - 1;
+        buildString += `<h6>${departmentData.categories[categoryId].season_discount}</h6>`
         productContainer.innerHTML += buildString
     }
 
 }
 
 
+seasonalDiscount.addEventListener("change", whichSeasonIsItDamnit);
+function whichSeasonIsItDamnit() {
+    var catWinter = [];
+    var catAutumn = [];
+    var catSpring = [];
 
+// Puts each item in the product container into an array
+    var priceHolder = productContainer.getElementsByClassName("product");
+    for (var i = 0; i < priceHolder.length; i++) {
+        if (priceHolder[i].innerHTML.indexOf("Winter") !== -1)
+            catWinter.push(priceHolder[i]);
+        } else if (priceHolder[i].innerHTML.indexOf("Autumn") !== -1) {
+            catAutumn.push(priceHolder[i]);
+        } else if (priceHolder[i].innerHTML.indexOf("Spring") !== -1) {
+            catWinter.push(priceHolder[i]);
+        }
+    console.log("priceHolder", priceHolder);
+    console.log("catWinter", catWinter);
 
+    var newPrice = 0;
+            if (seasonalDiscount.value === "winter") {
+                var winterDiscountPrice = catWinter.forEach(updatePriceWinter) 
+            } else if (seasonalDiscount.value === "autumn") {
+                var autumnDiscountPrice = catAutumn.forEach(updatePriceAutumn) 
+            } else if (seasonalDiscount.value === "spring") {
+                var springDiscountPrice = catSpring.forEach(updatePriceSpring) 
+            }
+        }
+}
+          
