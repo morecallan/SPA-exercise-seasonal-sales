60db560e926afdba65eefb19f07aad3a01c14063
Callan Morrison <callan.l.morrison@gmail.com>
2016-03-12 11:26:12 -0600

    completed seasonal discounts using DOM

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -3,16 +3,6 @@ var seasonalDiscount = document.getElementById("seasonalDiscount");
 
 
 
-
-
-
-
-
-
-
-
-
-
 // PRODUCT XHR
 //Step 1: Set up http req for products
 var myProductsReq = new XMLHttpRequest;
@@ -62,9 +52,10 @@ function domOutput(productData, departmentData) {
     for (var i = 0; i < productData.products.length; i++) {
         var buildString = "<div class='product'";
         buildString += `<h2> Product: ${productData.products[i].name} </h2>`
-        buildString += `<h5> Price: ${productData.products[i].price} </h5>`
+        buildString += `<h5> Price: <p class="price">${productData.products[i].price}<p> </h5>`
         var categoryId = productData.products[i].category_id - 1;
         buildString += `<h6>${departmentData.categories[categoryId].season_discount}</h6>`
+        buildString += `<h5 class="discountedPrice"> </h5>`
         productContainer.innerHTML += buildString
     }
 
@@ -72,7 +63,9 @@ function domOutput(productData, departmentData) {
 
 
 seasonalDiscount.addEventListener("change", whichSeasonIsItDamnit);
+
 function whichSeasonIsItDamnit() {
+    clearDiscounts();
     var catWinter = [];
     var catAutumn = [];
     var catSpring = [];
@@ -80,24 +73,53 @@ function whichSeasonIsItDamnit() {
 // Puts each item in the product container into an array
     var priceHolder = productContainer.getElementsByClassName("product");
     for (var i = 0; i < priceHolder.length; i++) {
-        if (priceHolder[i].innerHTML.indexOf("Winter") !== -1)
+        if (priceHolder[i].innerHTML.indexOf("Winter") !== -1) {
             catWinter.push(priceHolder[i]);
         } else if (priceHolder[i].innerHTML.indexOf("Autumn") !== -1) {
             catAutumn.push(priceHolder[i]);
         } else if (priceHolder[i].innerHTML.indexOf("Spring") !== -1) {
             catWinter.push(priceHolder[i]);
         }
-    console.log("priceHolder", priceHolder);
-    console.log("catWinter", catWinter);
+    }
 
-    var newPrice = 0;
-            if (seasonalDiscount.value === "winter") {
-                var winterDiscountPrice = catWinter.forEach(updatePriceWinter) 
-            } else if (seasonalDiscount.value === "autumn") {
-                var autumnDiscountPrice = catAutumn.forEach(updatePriceAutumn) 
-            } else if (seasonalDiscount.value === "spring") {
-                var springDiscountPrice = catSpring.forEach(updatePriceSpring) 
-            }
-        }
+    if (seasonalDiscount.value === "winter") {
+        var winterDiscountPrice = catWinter.forEach(updatePriceWinter) 
+    } else if (seasonalDiscount.value === "autumn") {
+        var autumnDiscountPrice = catAutumn.forEach(updatePriceAutumn) 
+    } else if (seasonalDiscount.value === "spring") {
+        var springDiscountPrice = catSpring.forEach(updatePriceSpring) 
+    }
+
+    function updatePriceWinter(winterProduct) {
+        var price = parseFloat(winterProduct.getElementsByClassName("price")[0].innerHTML);
+        var newPrice = (price * .9).toFixed(2);
+        var discountArea = winterProduct.getElementsByClassName("discountedPrice")[0]
+        discountArea.innerHTML = newPrice;
+    }
+
+    function updatePriceAutumn(autumnProduct) {
+        var price = parseFloat(autumnProduct.getElementsByClassName("price")[0].innerHTML);
+        var newPrice = (price * .75).toFixed(2);
+        var discountArea = autumnProduct.getElementsByClassName("discountedPrice")[0]
+        discountArea.innerHTML = newPrice;
+    }
+
+    function updatePriceSpring(springProduct) {
+        var price = parseFloat(springProduct.getElementsByClassName("price")[0].innerHTML);
+        var newPrice = (price * .85).toFixed(2);
+        var discountArea = springProduct.getElementsByClassName("discountedPrice")[0]
+        discountArea.innerHTML = newPrice;
+    }
+    
+
 }
+
+function clearDiscounts() {
+    var discounts = document.getElementsByClassName("discountedPrice")
+    for (var i = 0; i < discounts.length; i++ ) {
+        discounts[i].innerHTML = "";
+    }
+}
+
+
           
